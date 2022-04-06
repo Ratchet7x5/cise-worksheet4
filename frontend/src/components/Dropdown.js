8a170007c6a7b728ca92e9700ddedc34f5e38ad9
Ratchet7x5 <36789694+Ratchet7x5@users.noreply.github.com>
2022-04-06 21:41:32 +1200

    Step 33: Pre-Backend (Functional Build)

--- frontend/src/components/Dropdown.js
+++ frontend/src/components/Dropdown.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import SEPractices from "../dummydata/SEPractices"
 
-  const optionItems = SEPractices.map((SEPractice) =>
-                <option key={SEPractice.practice}>{SEPractice.practice}</option>
-            );
-  const Dropdown = () => {
-    return (
-        <div>
-             <select>
-             <option value="">Select an SE Practice </option>
-                {optionItems}
-             </select>
-         </div>
-    )
-  }
-  export default Dropdown;
+import { useRef } from "react";
+import SEPractices from "../dummydata/SEPractices";
+
+const optionItems = SEPractices.map((SEPractice) => (
+  <option key={SEPractice.practice}>{SEPractice.practice}</option>
+));
+
+const Dropdown = ({ setSelectedPractice }) => {
+  const handleDropdownChange = (e) => {
+    setSelectedPractice(e.target.value);
+  };
+
+  return (
+    <div>
+      <select onChange={handleDropdownChange}>
+        <option value="">Select an SE Practice</option>
+        {optionItems}
+      </select>
+    </div>
+  );
+};
+export default Dropdown;
