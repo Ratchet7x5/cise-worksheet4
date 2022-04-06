d58afdb56b809720b11bb1a69a005ba696cd667d
Ratchet7x5 <36789694+Ratchet7x5@users.noreply.github.com>
2022-04-06 18:47:26 +1200

    Step 17.5: Fixed Repo (again)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,40 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, NavLink, Navigate} from "react-router-dom";
+import "./index.css"
 
 import Home from "./pages/Home";
 import SEPractice from "./pages/SE-Practice";
-import SubmitArticle from "./pages/Submit-Article"; 
+import SubmitArticle from "./pages/Submit-Article";
 import NotFoundPage from "./pages/404";
-const App = () =>  {
-    return (
-        <Router>
-        <div>
-          <h1>Software Engineering Practice Evidence Repository (SEPER)</h1>
-            <ul className="header">
-                <li><NavLink exact to = "/">Home</NavLink></li>
-                <li><NavLink to = "/SEPractice">Select the Practice</NavLink></li>
-                <li><NavLink to = "/SubmitArticle">Submit an Article</NavLink></li>
-            </ul>
-          <div className="content">
-            <Route exact path="/" component={Home}/>
-            <Route  path="/SEPractice" component={SEPractice}/>
-            <Route  path="/SubmitArticle" component={SubmitArticle}/>
-            <Route exact path="/404" component={NotFoundPage}/>
-            <Redirect to="/404" />
-          </div>
+
+const App = () => {
+  return (
+    <Router >
+      <div>
+        <h1>Software Engineering Practice Evidence Repository (SEPER)</h1>
+        <ul className="header">
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/SEPractice" >Select the Practice</NavLink>
+          </li>
+          <li>
+            <NavLink to="/SubmitArticle" >Submit an Article</NavLink>
+          </li>
+        </ul>
+        <div className="content">
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/SEPractice" element={<SEPractice />} />
+            <Route path="/SubmitArticle" element={<SubmitArticle />} />
+            <Route exact path="404" element={<NotFoundPage />} />
+            <Route path="/*" element={<Navigate to="404" replace />} />
+          </Routes>
         </div>
-        </Router>
-    );
-}
+      </div>
+    </Router>
+  );
+};
+
 export default App;
