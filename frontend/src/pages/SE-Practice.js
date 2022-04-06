190cd33727d84412e0b955b7641c48bcfd0efd5a
Ratchet7x5 <36789694+Ratchet7x5@users.noreply.github.com>
2022-04-06 19:00:48 +1200

    Step 25: Update SE-Practice.js, more Tables (in src/components/)

--- frontend/src/pages/SE-Practice.js
+++ frontend/src/pages/SE-Practice.js
@@ -1,13 +1,39 @@
-import React from "react";
- 
+import TableStyles from "../components/TableStyle";
+import Table from "../components/EvidenceTable";
+import TableColumns from "../components/TableColumns";
+import Dropdown from "../components/Dropdown";
+import { useEffect, useState } from "react";
+import { getAllArticles } from "../services/articleservice";
+
 const SEPractice = () => {
-    return (
-      <div>
-        <h2>Select SE Practice to get evidence for the claimed benefits</h2>
-        <p>There will be a way to select an SE practice from our SEPER repository here</p>
-        <p>Also the display table of evidence levels for different claims will show here for the SE practice selected</p>
-      </div>
-    );
-}
- 
+  const [selectedPractice, setSelectedPractice] = useState("");
+  const [articles, setArticles] = useState([]);
+
+  // Grab articles from MongoDB Atlas
+  useEffect(() => {
+    getAllArticles()
+      .then(articles => setArticles(articles))
+      .catch((err) => console.err(`Failed to get articles: ${err.message}`));
+  }, []);
+
+  const getArticles = () => {
+    switch (selectedPractice) {
+      case "Mob Programming":
+        return articles.filter(article => article.sepractice === "Mob Programming");
+      default:
+        return articles.filter(article => article.sepractice === "TDD");
+    }
+  }
+
+  return (
+    <div>
+      <h2>Select SE Practice to get evidence for the claimed benefits</h2>
+      <Dropdown setSelectedPractice={setSelectedPractice} />
+      <TableStyles>
+        <Table selectedPractice={selectedPractice} data={getArticles()} columns={TableColumns} />
+      </TableStyles>
+    </div>
+  );
+};
+
 export default SEPractice;
